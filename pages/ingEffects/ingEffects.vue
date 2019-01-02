0c4c45f6b5b0a7f11ffe94af25dd425422fdbed9
周雪荣 <2829387775@qq.com>
2019-01-02 22:09:07 +0800

    .。

--- pages/ingEffects/ingEffects.vue
+++ pages/ingEffects/ingEffects.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="aaa">
-    <div class="bbb">44</div>
+    <Input v-model="value" :type="file"/>
+    <Button type="primary" @click="sss">文本</Button>
+    <Input v-model="value2" type="textarea" :rows="30" class="input" />
+    <img :src="url"/>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      value: '',
+      value2: '',
+      url: ''
+    }
+  },
+  methods: {
+    sss () {
+      let file = this.files[0]
+      if (!/image\/\w+/.test(file.type)) {
+        alert('请确保文件为图像类型')
+        return false
+      }
+      let fileReader = new FileReader()
+      fileReader.readAsDataURL(file.files[0])
+      fileReader.onload = function () {
+        let imgBase64Data = this.result
+        this.value2 = imgBase64Data
+        this.url = imgBase64Data
+      }
+    }
+  }
+}
 </script>
 <style scoped lang="less">
 .aaa {
-  width: 200px;
-  height: 200px;
+  width: 100%;
+  margin-top: 200px;
   background-color: #f5ff;
   .bbb {
     width: 50px;
