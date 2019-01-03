a2fbf0c459ea04306d8cb71b27f5bf2d9fdd0149
周雪荣 <3124530200@qq.com>
2019-01-03 18:14:13 +0800

    0

--- pages/faceIdentify/faceIdentify.vue
+++ pages/faceIdentify/faceIdentify.vue
@@ -1,171 +1,146 @@
 <template>
-  <div class="aaa">
+  <div class="sss">
     <div class="bg">
       <bg/>
     </div>
-    <Row
-      class="code-row-bg"
-      type="flex"
-      justify="center"
-    >
-      <i-col
-        span="8"
-        class="ccc"
-      >
-        <div
-          v-for="item in uploadList"
-          :key="item.index"
-          class="demo-upload-list"
-        >
-          <template v-if="item.status === 'finished'">
-            <img :src="item.url">
-          </template>
-          <template v-else>
-            <Progress v-if="item.showProgress" :percent="item.percentage" hide-info>
-              上传图片开始制作
-            </Progress>
-          </template>
-        </div>
-        <Upload
-          ref="upload"
-          :show-upload-list="true"
-          :default-file-list="defaultList"
-          :on-success="handleSuccess"
-          :format="['jpg','jpeg','png']"
-          :max-size="2048"
-          :on-format-error="handleFormatError"
-          :on-exceeded-size="handleMaxSize"
-          multiple
-          type="drag"
-          action="//jsonplaceholder.typicode.com/posts/">
-          <div style="width: 58px;height:58px;line-height: 58px;">
-          </div>
-        </Upload>
-        <!-- <Modal title="View Image" v-model="visible">
-          <img :src="'https://o5wwk8baw.qnssl.com/' + imgName + '/large'" v-if="visible" style="width: 100%">
-        </Modal> -->
-      </i-col>
-      <!-- <i-col
-        span="8"
-        class="ccc"
-      >
-        <img 
-          class="filter-img"
-          src="~/assets/img/filter02.jpg"
-        >
-        <span class="filter-span">
-          <a>风景滤镜</a>
-        </span>
-      </i-col> -->
-    </Row>
+    <div class="content">
+      <input type="file" accept="image/*" @change="changeImg($event)">
+      <img :src="imgData" alt="">
+      <Button type="primary" @click="sss">一键美颜</Button>
+      <!-- <Input v-model="imgData" type="textarea" :rows="30" class="input" /> -->
+    </div>
   </div>
 </template>
 <script>
 import bg from '~/assets/bg.vue'
+import config from '~/assets/js/config.js'
+import axios from 'axios'
+import md5 from 'md5'
+import Qs from 'Qs'
 export default {
   components: {
     bg
   },
   data () {
     return {
-      defaultList: [
-        // {
-        //   'name': 'a42bdcc1178e62b4694c830f028db5c0',
-        //   'url': 'https://o5wwk8baw.qnssl.com/a42bdcc1178e62b4694c830f028db5c0/avatar'
-        // }
-      ],
-      imgName: '',
-      visible: false,
-      uploadList: [],
-      base64_code: ''
+      imgData: '',
+      appKey: 'T7Ogcjb7gfuhrvuW',
+      appId: '2110945056'
     }
   },
   methods: {
-    // handleView (name) {
-    //   this.imgName = name
-    //   this.visible = true
-    // },
-    // handleRemove (file) {
-    //   const fileList = this.$refs.upload.fileList
-    //   this.$refs.upload.fileList.splice(fileList.indexOf(file), 1)
-    // },
-    handleSuccess (res, file) {
-      file.url = 'https://o5wwk8baw.qnssl.com/7eb99afb9d5f317c912f08b5212fd69a/avatar'
-      file.name = '7eb99afb9d5f317c912f08b5212fd69a'
+    // 上传图片并转码
+    changeImg: function (e) {
+      const _this = this
+      const imgLimit = 1024 // 图片限制大小
+      const files = e.target.files
+      const image = new Image() // 浏览器缓存图片
+      if (files.length > 0) {
+        let dd = 0
+        const timer = setInterval(function () {
+          // 判断上传文件图片格式
+          if (files.item(dd).type !== 'image/png' &&
+            files.item(dd).type !== 'image/jpeg' &&
+            files.item(dd).type !== 'image/gif') {
+            return false
+          }
+          // 图片大小
+          if (files.item(dd).size > imgLimit * 102400) {
+            // to do sth
+          } else {
+            image.src = window.URL.createObjectURL(files.item(dd))
+            image.onload = function () {
+              // 默认按比例压缩
+              let [w, h] = [image.width, image.height]
+              let scale = w / h
+              w = 400
+              h = w / scale
+              // 默认图片质量为0.7，quality值越小，所绘制出的图像越模糊
+              let quality = 0.7
+              // 生成canvas
+              let canvas = document.createElement('canvas')
+              let ctx = canvas.getContext('2d')
+              // 创建属性节点
+              let anw = document.createAttribute('width')
+              anw.nodeValue = w
+              let anh = document.createAttribute('height')
+              anh.nodeValue = h
+              canvas.setAttributeNode(anw)
+              canvas.setAttributeNode(anh)
+              ctx.drawImage(image, 0, 0, w, h)
+              let ext = image.src.substring(image.src.lastIndexOf('.') + 1).toLowerCase() // 图片格式
+              let base64 = canvas.toDataURL('image/' + ext, quality)
+              _this.imgData = base64
+            }
+          }
+          if (dd < files.length - 1) {
+            dd++
+          } else {
+            clearInterval(timer)
+          }
+        }, 1000)
+      }
     },
-    handleFormatError (file) {
-      this.$Notice.warning({
-        title: 'The file format is incorrect',
-        desc: 'File format of ' + file.name + ' is incorrect, please select jpg or png.'
+    // 网络请求
+    sss () {
+      let params = {
+        app_id: this.appId,
+        time_stamp: config.timeStamp,
+        nonce_str: config.nonceStr,
+        filter: 1,
+        image: this.imgData
+      }
+      // 将<key, value>请求参数对按key进行字典升序排序，得到有序的参数对列表N
+      const N = Object.keys(params).sort()
+      console.log(N)
+      // 将列表N中的参数对按URL键值对的格式拼接成字符串，得到字符串T（如：key1=value1&key2=value2），
+      const TT = N.map(key => {
+        const value = params[key]
+        return `${key}=${encodeURIComponent(value)}`
       })
-    },
-    handleMaxSize (file) {
-      this.$Notice.warning({
-        title: 'Exceeding file size limit',
-        desc: 'File  ' + file.name + ' is too large, no more than 2M.'
-      })
+      console.log(TT)
+      // URL键值拼接过程value部分需要URL编码，URL编码算法用大写字母，例如%E8，而不是小写%e8
+      const T = TT.join('&')
+      console.log(T)
+      // 将应用密钥以app_key为键名，组成URL键值拼接到字符串T末尾，得到字符串S（如：key1=value1&key2=value2&app_key=密钥)
+      const S = `${T}&app_key=${this.appKey}`
+      console.log(S)
+      // 对字符串S进行MD5运算，将得到的MD5值所有字符转换成大写，得到接口请求签名
+      const sign = md5(S).toUpperCase()
+      console.log(sign)
+      // params.forEach(item => {
+      //   item.sign = sign
+      // })
+      const paramter = {
+        ...params,
+        sign
+      }
+      console.log(paramter)
+      axios.post('/fcgi-bin/ptu/ptu_imgfilter', Qs.stringify(paramter))
+        .then(res => {
+          console.log(res.data.data.trans_text)
+        }).catch(err => {
+          console.log(err)
+        })
     }
-    // handleBeforeUpload () {
-    //   const check = this.uploadList.length < 5
-    //   if (!check) {
-    //     this.$Notice.warning({
-    //       title: 'Up to five pictures can be uploaded.'
-    //     })
-    //   }
-    //   return check
-    // }
-  },
-  mounted () {
-    this.uploadList = this.$refs.upload.fileList
   }
 }
 </script>
 <style scoped lang="less">
-.bg {
-  position: fixed;
-  z-index: -1000;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-}
-.code-row-bg {
-  margin-top: 100px;
-}
-.demo-upload-list{
-  display: inline-block;
-  width: 300px;
-  height: 300px;
-  text-align: center;
-  line-height: 60px;
-  border: 1px solid transparent;
-  border-radius: 4px;
-  overflow: hidden;
-  background: #fff;
-  position: relative;
-  box-shadow: 0 1px 1px rgba(0,0,0,.2);
-  margin-right: 4px;
-}
-.demo-upload-list img{
+.sss {
   width: 100%;
-  height: 100%;
+  text-align: center;
+  .bg {
+    position: fixed;
+    z-index: -1000;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+  }
+  .content {
+    margin-top: 200px;
+  }
 }
-.demo-upload-list-cover{
-  display: none;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: rgba(0,0,0,.6);
-}
-.demo-upload-list:hover .demo-upload-list-cover{
-  display: block;
-}
-.demo-upload-list-cover i{
-  color: #fff;
-  font-size: 20px;
-  cursor: pointer;
-  margin: 0 2px;
-}
+
 </style>
